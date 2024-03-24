056504c013458b8a93868c32ae79edfb75477a62
SRISHTI1802 <srishtiaggarwal1802@gmail.com>
2024-03-24 16:01:23 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,7 +45,44 @@ class ModelTrainer:
                 #"KNN Regeressor" : KNeighborsRegressor(),
                 "XGBOOST" : XGBRegressor()
             }
-            model_report:dict = evaluate_models(X_train,y_train,X_test,y_test,models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regressor":{},
+                "XGBOOST":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Catboost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict = evaluate_models(X_train,y_train,X_test,y_test,models = models,params = params)
             #Finding the best model:
             best_model_score = max(sorted(model_report.values()))
             #getting the best model name;
@@ -69,7 +106,4 @@ class ModelTrainer:
             
 
             
-            
-        except:
-            pass
 
--- src/utlis.py
+++ src/utlis.py
@@ -7,6 +7,7 @@ import numpy
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 #defining a funciton called save_object to save the preprocessor as pickle fath at speicifed path
 def save_object(file_path,obj):
@@ -17,12 +18,23 @@ def save_object(file_path,obj):
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report =  {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv = 3)
+            gs.fit(X_train,y_train)
+
+            #setting the best parameters
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+
+
+
+            #model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred =  model.predict(X_test)
             #calculating the r2_score oor the train and test data for  the model
